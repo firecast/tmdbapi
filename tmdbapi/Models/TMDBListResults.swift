f4b5d6f4cbf4c3111ff9c38d6547d4ba2ca2ab35
Amit Prabhu <amit.dp180@gmail.com>
2017-03-25 15:39:59 +0530

    Add TMDBTVListResult model

--- tmdbapi/Models/TMDBListResults.swift
+++ tmdbapi/Models/TMDBListResults.swift
@@ -9,14 +9,14 @@
 import Foundation
 import SwiftyJSON
 
-public protocol TMDBListResult {
+public protocol TMDBListResultProtocol {
     var title: String { get }
     var description: String { get }
     var imagePath: String { get }
     var mediaType: TMDBMediaType { get }
 }
 
-public class TMDBMovieListResult: TMDBListResult {
+public class TMDBMovieListResult: TMDBListResultProtocol {
     public let posterPath: String?
     
     public let isAdult: Bool
@@ -88,8 +88,79 @@ public class TMDBMovieListResult: TMDBListResult {
     
 }
 
-public class TMDBTVListResult {
+public class TMDBTVListResult: TMDBListResultProtocol {
+    public let posterPath: String?
     
+    public let overview: String
+    public let firstAirDate: Date
+    public let originalName: String
+    public let genres: [TMDBTVGenre]
+    public let originCountries: [String]
+    
+    public let id: UInt
+    
+    public let originalLanguage: String
+    public let name: String
+    public let backdropPath: String
+    public let popularity: NSNumber
+    public let voteCount: Int
+    
+    public let voteAverage: NSNumber
+    
+    // MARK: TMDBListResult
+    public var title: String {
+        get {
+            return self.name
+        }
+    }
+    public var description: String {
+        get {
+            return self.overview
+        }
+    }
+    public var imagePath: String {
+        get {
+            return self.backdropPath
+        }
+    }
+    public var mediaType : TMDBMediaType
+    
+    internal init?(json: JSON) {
+        guard let mediaType = TMDBMediaType(rawValue: json["media_type"].stringValue) else {
+            return nil
+        }
+        self.mediaType = mediaType
+        
+        self.posterPath = json["poster_path"].string
+        self.overview = json["overview"].stringValue
+        self.firstAirDate = TMDBAPIStatic.dateFormatter.date(from: json["first_air_date"].stringValue)!
+        
+        self.originalName = json["original_name"].stringValue
+        self.id = json["id"].uIntValue
+        self.originalLanguage = json["original_language"].stringValue
+        self.name = json["name"].stringValue
+        self.backdropPath = json["backdrop_path"].stringValue
+        self.popularity = json["popularity"].numberValue
+        self.voteCount = json["vote_count"].intValue
+        
+        self.voteAverage = json["vote_average"].numberValue
+        
+        // Genres
+        var genres = [TMDBTVGenre]()
+        for (_, genreID):(String, JSON) in json["genere_ids"] {
+            if let genre = TMDBTVGenre(rawValue: genreID.intValue) {
+                genres.append(genre)
+            }
+        }
+        self.genres = genres
+        
+        // Origin countries
+        var originCountries = [String]()
+        for (_, country):(String, JSON) in json["origin_country"] {
+            originCountries.append(country.stringValue)
+        }
+        self.originCountries = originCountries
+    }
     
 }
 
