4fb7f05b5fc369d4fc3b9d4203f61b0562bf374e
Amit Prabhu <amit.dp180@gmail.com>
2017-03-25 15:10:18 +0530

    Add TMDBMovieGenre enum

---
+++ tmdbapi/Models/TMDBGenres.swift
@@ -0,0 +1,31 @@
+//
+//  TMDBGenres.swift
+//  tmdbapi
+//
+//  Created by Amit Prabhu on 3/25/17.
+//  Copyright © 2017 Amit Prabhu. All rights reserved.
+//
+
+import Foundation
+
+public enum TMDBMovieGenre: Int {
+    case action = 28
+    case adventure = 12
+    case animation = 16
+    case comedy = 35
+    case crime = 80
+    case documentary = 99
+    case drama = 18
+    case family = 10751
+    case fantasy = 14
+    case history = 36
+    case horror = 27
+    case music = 10402
+    case mystery = 9648
+    case romance = 10749
+    case scienceFiction = 878
+    case tvMovie = 10770
+    case thriller = 53
+    case war = 10752
+    case western = 37
+}
