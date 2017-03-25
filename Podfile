c9ee420517f26d2a380086ecb496edf88939507e
Amit Prabhu <amit.dp180@gmail.com>
2017-03-25 13:19:03 +0530

    Add skeleton framework

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '10.0'
+use_frameworks!
+
+target 'tmdbapi' do
+  # Pods for tmdbapi
+  pod 'Alamofire', '~> 4.4'
+  pod 'SwiftyJSON'
+
+
+  target 'tmdbapiTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ tmdbapi/tmdbapi.h
@@ -0,0 +1,19 @@
+//
+//  tmdbapi.h
+//  tmdbapi
+//
+//  Created by Amit Prabhu on 3/25/17.
+//  Copyright © 2017 Amit Prabhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tmdbapi.
+FOUNDATION_EXPORT double tmdbapiVersionNumber;
+
+//! Project version string for tmdbapi.
+FOUNDATION_EXPORT const unsigned char tmdbapiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tmdbapi/PublicHeader.h>
+
+
